717f86089f565073af5ed058e75b2f0839606d0b
Sandeep Desaraju <71257134+sandeep-dss@users.noreply.github.com>
2023-11-14 12:48:09 +0530

    updated cors method

--- backend/index.js
+++ backend/index.js
@@ -6,24 +6,16 @@ import cors from 'cors';
 
 const app = express();
 
-// Middleware for parsing request body
+
 app.use(express.json());
 
-// Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
+// Middleware for handling CORS 
 app.use(cors());
-// Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
+
 
 app.get('/', (request, response) => {
   console.log(request);
-  return response.status(234).send('Welcome To MERN Stack Tutorial');
+  return response.status(234).send('Server is running');
 });
 
 app.use('/books', booksRoute);
